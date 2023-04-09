6152891181a77a5e7439ea339013f409119abd49
GrzegorzTworek <gt.grzegorz.tworek@gmail.com>
2023-04-09 20:42:55 +0200

    Project commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,62 @@
-// import { useRef } from "react";
-// import { Canvas, useFrame } from "@react-three/fiber";
-// import { CircleGeometry, MeshBasicMaterial, Mesh } from "three";
+import { useRef } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { CuboidCollider, Physics, RigidBody } from "@react-three/rapier";
 
-// function Circle() {
-//   const radius = 4;
-//   const segments = 64;
-//   const geometry = new CircleGeometry(radius, segments);
-//   const material = new MeshBasicMaterial({
-//     color: 0xffffff,
-//     // transparent: true, // włączenie przezroczystości
-//     // opacity: 1, // ustawienie przezroczystości na 50%
-//     // side: 2, // renderowanie odwróconych powierzchni
-//     // wireframe: true, // włączenie krawędzi
-//     // wireframeLinewidth: 20, // ustawienie grubości krawędzi na 5 pikseli
-//   });
-//   const meshRef = useRef(); // utworzenie referencji do Mesh
+function Ball() {
+  const ref = useRef();
+  const { viewport } = useThree();
+  const onCollisionEnter = () => (
+    ref.current.setTranslation({ x: 0, y: 0, z: 0 }),
+    ref.current.setLinvel({ x: 0, y: 0, z: 0 })
+  );
+  return (
+    <>
+      <RigidBody ref={ref} colliders="ball" mass={1}>
+        <mesh>
+          <sphereGeometry args={[0.1, 16, 16]} />
 
-//   useFrame(() => {
-//     // Aktualizacja pozycji Mesh w każdej klatce animacji
-//     meshRef.current.position.z = -1;
-//   });
+          <meshStandardMaterial />
+        </mesh>
+      </RigidBody>
+      <RigidBody
+        type="fixed"
+        colliders={false}
+        position={[0, -viewport.height, 0]}
+        restitution={2.1}
+        onCollisionEnter={onCollisionEnter}
+      >
+        <CuboidCollider args={[-5, 5, 0]} />
+      </RigidBody>
+    </>
+  );
+}
 
-//   return <mesh ref={meshRef} geometry={geometry} material={material} />;
-// }
+const balls = Array.from({ length: 20 }, (_, index) => <Ball key={index} />);
 
-// export default function App() {
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         height: "100vh",
-//       }}
-//     >
-//       <Canvas style={{ background: "black" }}>
-//         <Circle />
-//       </Canvas>
-//     </div>
-//   );
-// }
+const App = () => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+    }}
+  >
+    <Canvas style={{ background: "black" }}>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 5]} />
+      <Physics gravity={[0, -5, 0]}>
+        <RigidBody colliders="trimesh" type="fixed" restitution={2.1}>
+          <mesh>
+            <ringGeometry args={[2.9, 3, 64]} attach="geometry" />
+            <meshBasicMaterial color="red" attach="material" />
+          </mesh>
+        </RigidBody>
+
+        {balls}
+      </Physics>
+    </Canvas>
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Appp from "./Appp";
-import Ap from "./Ap";
-import AA from "./AA";
-import Project from "./Project";
-// import Pr from "./Pr";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
-// root.render(<AA />);
-root.render(<Project />);
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(<App />);
